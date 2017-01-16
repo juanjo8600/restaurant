c20bfe7e6d2252ec0885d5b99074ac4b14dac799
jj.jimenez <jj.jimenez@barceloviajes.com>
2017-01-16 16:52:02 +0100

    Buy bag webservice configuration test

--- service/webservice/src/main/java/org/restaurant/webservice/buybag/buy/BuyBagResponse.java
+++ service/webservice/src/main/java/org/restaurant/webservice/buybag/buy/BuyBagResponse.java
@@ -1,7 +1,22 @@
 package org.restaurant.webservice.buybag.buy;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
 /**
  * Created by jj.jimenez on 11/01/2017.
  */
-public class BuyBagResponse {
+@XmlRootElement(name = "buyBagResponse1") public class BuyBagResponse implements Serializable {
+
+  private String prop;
+
+   @XmlElement(name = "prop1")
+   public String getProp() {
+	  return prop;
+   }
+
+   public void setProp(String prop) {
+	  this.prop = prop;
+   }
 }
