efbe0384e4bad5a4210de3b1619f0dc42cab63aa
jj.jimenez <jj.jimenez@barceloviajes.com>
2017-01-17 17:50:48 +0100

    With this change application starts

--- service/webservice/src/main/java/org/restaurant/webservice/buybag/buy/BuyBagResponse.java
+++ service/webservice/src/main/java/org/restaurant/webservice/buybag/buy/BuyBagResponse.java
@@ -1,18 +1,21 @@
 package org.restaurant.webservice.buybag.buy;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 
 /**
  * Created by jj.jimenez on 11/01/2017.
  */
-@XmlRootElement(name = "buyBagResponse1") public class BuyBagResponse implements Serializable {
+@XmlRootElement @XmlType(name = "BuyBagResponse") public class BuyBagResponse
+   implements Serializable {
 
-  private String prop;
+   private String prop;
 
-   @XmlElement(name = "prop1")
-   public String getProp() {
+   @XmlElement(name = "Prop1") public String getProp() {
 	  return prop;
    }
 
