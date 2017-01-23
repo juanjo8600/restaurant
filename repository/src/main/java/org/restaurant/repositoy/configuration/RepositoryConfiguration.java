3a015ffd4bcdf393034221cf09615383054e0e71
jj.jimenez <jj.jimenez@barceloviajes.com>
2017-01-23 09:20:28 +0100

    Link webservice with other layers.

--- repository/src/main/java/org/restaurant/repositoy/configuration/RepositoryConfiguration.java
+++ repository/src/main/java/org/restaurant/repositoy/configuration/RepositoryConfiguration.java
@@ -2,9 +2,11 @@ package org.restaurant.repositoy.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Created by jj.jimenez on 23/01/2017.
  */
-@Configuration @ComponentScan(basePackages = "org.restaurant.repository") public class RepositoryConfiguration {
+@Configuration @ComponentScan(basePackages = "org.restaurant.repository") @EnableJpaRepositories @EnableTransactionManagement public class RepositoryConfiguration {
 }
